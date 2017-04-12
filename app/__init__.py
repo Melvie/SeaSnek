545d47b40511803d346dd92de1020d5ce8050962
Melvie <melvie.tarr@gmail.com'git config --global user.name Melviegit config --global user.email "melvie.tarr@gmail.com>
2017-04-11 20:40:49 -0700

    fixed db problem

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, request, redirect, jsonify, abort
 from flask_restful import Resource, Api, fields, marshal, reqparse
 
-
 app = Flask(__name__)
 
 app.config.from_object('config')
--- db_create.py
+++ db_create.py
@@ -3,7 +3,7 @@ import os.path
 from migrate.versioning import api
 from config import SQLALCHEMY_DATABASE_URI
 from config import SQLALCHEMY_MIGRATE_REPO
-from app import db
+from app import db, models
 
 
 db.create_all()
